886bf680cf6af204106dcc798d651a726c210aaf
hwalker928 <freelance@harrydev.me>
2021-06-12 22:21:55 +0100

    Update generate.js

--- functions/generate.js
+++ functions/generate.js
@@ -1,5 +1,6 @@
 function generate() {
     var passwordLength = Math.floor(Math.random() * 5) + 8;
+    console.log(passwordLength);
     var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!$%^&*()-_=+';
     var pass = "";
     for (i = 1; i <= passwordLength; i++) {
