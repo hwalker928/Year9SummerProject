1f7af21d5a91c5f9f5b17733724f63a7adae2d85
hwalker928 <freelance@harrydev.me>
2021-06-12 16:43:39 +0100

    Generate

--- functions/generate.js
+++ functions/generate.js
@@ -1,4 +1,10 @@
 function generate() {
     var passwordLength = Math.floor(Math.random() * 12) + 8;
     var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!$%^&*()-_=+';
+    var pass = "";
+    for (i = 1; i <= passwordLength; i++) {
+        var char = Math.floor(Math.random()* characters.length + 1);
+        pass += str.charAt(char)
+    }
+    document.getElementById("final_pass").value = pass;
 }
