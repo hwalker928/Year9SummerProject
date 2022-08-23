2587c2b3371cc6567baddfdaabd53963cacebbcb
harry.w <harry.w@sparked.support>
2022-08-23 22:22:02 +0100

    Update code

--- functions/check.js
+++ functions/check.js
@@ -1,31 +1,34 @@
 function check() {
-    var matchesregex = /^[a-zA-Z0-9!$%^&*()-_=+]+$/.test(document.getElementById("pswd_to_check").value);
-    if (25>document.getElementById("pswd_to_check").value.length) { if (document.getElementById("pswd_to_check").value.length>7) { var correctlength = true; } else { var correctlength = false; } } else { var correctlength = false; }
-    var ucl = /^(?=.*[A-Z])/.test(document.getElementById("pswd_to_check").value);
-    var lcl = /^(?=.*[a-z])/.test(document.getElementById("pswd_to_check").value);
-    var digit = /^(?=.*[0-9])/.test(document.getElementById("pswd_to_check").value);
-    var allowedsymbols = /^(?=.*[!$%^&*()_=+-])/.test(document.getElementById("pswd_to_check").value);
-    var onlyLetters = /^[a-zA-Z]+$/.test(document.getElementById("pswd_to_check").value);
-    var onlyDigits = /^[0-9]+$/.test(document.getElementById("pswd_to_check").value);
-    var onlySymbols = /^[!$%^&*()_=+-]+$/.test(document.getElementById("pswd_to_check").value);
+    let correctlength;
+    let points;
+    let strength;
+    let stringCheck;
+    let extraifall;
+
+    const matchesregex = /^[a-zA-Z0-9!$%^&*()-_=+]+$/.test(document.getElementById("pswd_to_check").value);
+    if (25>document.getElementById("pswd_to_check").value.length) { correctlength = document.getElementById("pswd_to_check").value.length > 7; } else { correctlength = false; }
+    const ucl = /^(?=.*[A-Z])/.test(document.getElementById("pswd_to_check").value);
+    const lcl = /^(?=.*[a-z])/.test(document.getElementById("pswd_to_check").value);
+    const digit = /^(?=.*[0-9])/.test(document.getElementById("pswd_to_check").value);
+    const allowedsymbols = /^(?=.*[!$%^&*()_=+-])/.test(document.getElementById("pswd_to_check").value);
+    const onlyLetters = /^[a-zA-Z]+$/.test(document.getElementById("pswd_to_check").value);
+    const onlyDigits = /^[0-9]+$/.test(document.getElementById("pswd_to_check").value);
+    const onlySymbols = /^[!$%^&*()_=+-]+$/.test(document.getElementById("pswd_to_check").value);
 
     const qwerty1 = ["qwe", "wer", "ert", "rty", "tyu", "yui", "uio", "iop"];
     const qwerty2 = ["asd", "sdf", "dfg", "fgh", "ghj", "hjk", "jkl"];
     const qwerty3 = ["zxc", "xcv", "cvb", "vbn", "bnm"];
     console.clear();
 
-    if (matchesregex&correctlength&ucl&lcl&digit&allowedsymbols == 1) {
-        var extraifall = true;
-    } else {
-        var extraifall = false;
-    }
+    extraifall = !!(matchesregex && correctlength && ucl && lcl && digit && allowedsymbols);
 
     if (!(correctlength)) {
         document.getElementById("points").innerHTML = "Your password must be between 8 and 24 characters."
     } else if (!(matchesregex)) {
         document.getElementById("points").innerHTML = "Your password may only contain the following symbols: ! $ % ^ & * ( ) - _ = +"
     } else {
-        var points = document.getElementById("pswd_to_check").value.length;
+        points = document.getElementById("pswd_to_check").value.length;
+
         if (ucl) { points += 5 }
         if (lcl) { points += 5 }
         if (digit) { points += 5 }
@@ -35,48 +38,35 @@ function check() {
         if (onlyDigits) { points += -5 }
         if (onlySymbols) { points += -5 }
 
-
         for (i = 0, len = qwerty1.length, text = ""; i < len; i++) {
-            var stringCheck = qwerty1[i];
+            stringCheck = qwerty1[i];
             if(document.getElementById("pswd_to_check").value.toLowerCase().includes(stringCheck)) {
                points += -5
             }
         }
+
         for (i = 0, len = qwerty2.length, text = ""; i < len; i++) {
-            var stringCheck = qwerty2[i];
+            stringCheck = qwerty2[i];
             if(document.getElementById("pswd_to_check").value.toLowerCase().includes(stringCheck)) {
                points += -5
             }
         }
+
         for (i = 0, len = qwerty3.length, text = ""; i < len; i++) {
-            var stringCheck = qwerty3[i];
+            stringCheck = qwerty3[i];
             if(document.getElementById("pswd_to_check").value.toLowerCase().includes(stringCheck)) {
                points += -5
             }
         }
 
         if (points > 20) {
-            var strength = "Strong";
+            strength = "Strong";
         } else if (points < 1) {
-            var strength = "Weak";
+            strength = "Weak";
         } else {
-            var strength = "Medium";
+            strength = "Medium";
         }
-       
 
         document.getElementById("points").innerHTML = "<b>Password strength:</b> " + strength + " (" + points + ")";
     }
-
-
-    console.log("Matches character regex? " + matchesregex);
-    console.log("Good length? " + correctlength);
-    console.log("Upper case letter? " + ucl);
-    console.log("Lower case letter? " + lcl);
-    console.log("Digit? " + digit);
-    console.log("Allowed symbols? " + allowedsymbols);
-    console.log("All true? " + extraifall);
-    console.log("Only letters? " + onlyLetters);
-    console.log("Only digits? " + onlyDigits);
-    console.log("Only symbols? " + onlySymbols);
-    console.log("Total points: " + points)
 }
